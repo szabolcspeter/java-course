6e541ee8a66c18aab550c53393bd5a008cbebd35
Szabolcs József Péter <Szabolcs-Jozsef.Peter@t-systems.com>
2025-12-04 12:49:56 +0100

    ShortCircuitingAndLoopFusion

---
+++ src/szabi/learnjava/streams/ShortCircuitingAndLoopFusion.java
@@ -0,0 +1,39 @@
+package szabi.learnjava.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ShortCircuitingAndLoopFusion {
+
+    public static void main(String[] args) {
+
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Being and Time", "Heidegger", 560, Type.PHILOSOPHY));
+        books.add(new Book("The Trial", "Franz Kafka", 240, Type.NOVEL));
+        books.add(new Book("Death on the Nile", "Agatha Christie", 370, Type.THRILLER));
+        books.add(new Book("Ancient Greece", "Robert F.", 435, Type.HISTORY));
+        books.add(new Book("Ancient Rome", "Robert F.", 860, Type.HISTORY));
+        books.add(new Book("Death of Virgil", "Hermann Broch", 590, Type.NOVEL));
+        books.add(new Book("The Stranger", "Albert Camus", 560, Type.NOVEL));
+
+        // short-circuiting and loop fusion
+        // filter() and map() are different operations, they
+        // are merged into the same pass (loop fusion)
+        // short-circuiting: some operations don't need to process the whole
+        // stream to produce a result
+        // here we are looking for just 2 items - so the algorithm
+        // terminates after finding 2 items!
+        List<String> longestBooks = books.stream()
+                .filter(p -> {
+                    System.out.println("Filtering " + p.getTitle());
+                    return p.getPages() > 500;
+                })
+                .map(b -> {
+                    System.out.println("Mapping " + b.getTitle());
+                    return b.getTitle();
+                })
+                .limit(2)
+                .collect(Collectors.toList());
+    }
+}
